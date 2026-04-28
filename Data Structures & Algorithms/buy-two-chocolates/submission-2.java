d8ec79ea7cd4e9d7ca97d6f16341359820a97225
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2026-04-28 16:14:30 +0530

    Add: buy-two-chocolates - submission-2

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-2.java
@@ -0,0 +1,27 @@
+// class Solution {
+//     public int buyChoco(int[] prices, int money) {
+//         Arrays.sort(prices);
+//         int tmp=money;
+//         money-=prices[0];
+//         money-=prices[1];
+//         return money<0?tmp:money;
+//     }
+// }
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int p : prices) {
+            if (p < min1) {
+                min2 = min1;
+                min1 = p;
+            } else if (p < min2) {
+                min2 = p;
+            }
+        }
+
+        int cost = min1 + min2;
+        return (money >= cost) ? (money - cost) : money;
+    }
+}
